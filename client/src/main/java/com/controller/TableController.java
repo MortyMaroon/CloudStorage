f073e4bd6537e1177d7aa9cbcd640e026c0ea963
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-11-29 04:38:05 +0300

    Внесены небольшие доработки

--- client/src/main/java/com/controller/TableController.java
+++ client/src/main/java/com/controller/TableController.java
@@ -4,6 +4,9 @@ import com.utils.FileInfo;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.nio.file.*;
 import java.time.format.DateTimeFormatter;
 
@@ -11,7 +14,31 @@ public class TableController{
     public static void makeTable(TableView<FileInfo> tableView) {
         TableColumn<FileInfo, String> fileTypeColumn = new TableColumn<>("Type");
         fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
+//        fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
         fileTypeColumn.setPrefWidth(20);
+        fileTypeColumn.setCellFactory(column -> new TableCell<FileInfo, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                System.out.println(item);
+                super.updateItem(item, empty);
+                setText(null);
+                if (item == null || empty) {
+                    setStyle("");
+                    setGraphic(null);
+                } else {
+                    ImageView iv = new ImageView("images/file.png");
+                    iv.setFitHeight(15);
+                    iv.setFitWidth(15);
+                    if (item.equals("U")) {
+                        iv.setImage(new Image("images/up.png"));
+                    }
+                    if (item.equals("D")) {
+                        iv.setImage(new Image("images/folder.png"));
+                    }
+                    setGraphic(iv);
+                }
+            }
+        });
 
         TableColumn<FileInfo, String> fileNameColumn = new TableColumn<>("Name");
         fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
